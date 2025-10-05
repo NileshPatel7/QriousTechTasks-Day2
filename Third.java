96a26e01926b5a52d8e40acd084dd306e6466384
Nilesh Panchotiya <109101619+NileshPatel7@users.noreply.github.com>
2025-10-05 22:03:17 +0530

    Update Third.java

--- Third.java
+++ Third.java
@@ -2,20 +2,21 @@ package day2;
 
 public class Third {
     public static int findHighest(int []ar) {
-        int max=-888888;
+        int max=0;
         if (ar.length == 0)
         {
             System.out.println("No Value in Array");
             return 0;
         }
-        for(int i=0;i<ar.length;i++){
-            if(max<ar[i])
-                max=ar[i];
+        for(int i=0;i<ar.length-1;i++){
+            if(ar[i]<ar[i+1])
+                if(max<ar[i+1] || i==0)
+                    max=ar[i+1];
         }
         return max;
     }
     public static void main(String[] args) {
-        int []ar={5,12,91,43,7,82,35};
+        int []ar={-9999999,-8888888,-7777777,1203};
         System.out.println(findHighest(ar));
     }
 }
